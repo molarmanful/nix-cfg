34ec76ea8c2766a3f31855bb1cf3e66c25896c00
Ben Pang <ben@benpa.ng>
2025-01-09 22:45:16 +0000

    tofi

--- hm/sway/default.nix
+++ hm/sway/default.nix
@@ -30,4 +30,21 @@
     '';
 
   };
+
+  programs.tofi = {
+    enable = true;
+    settings = {
+      width = "100%";
+      height = "100%";
+      border-width = 0;
+      outline-width = 0;
+      padding-left = "35%";
+      padding-top = "35%";
+      result-spacing = 25;
+      font = "monospace";
+      background-color = "#000a";
+      hint-font = false;
+      ascii-input = true;
+    };
+  };
 }
