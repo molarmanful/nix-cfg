f27a421ab13a8c45b17ab55c94d06981cbe2ccfb
molarmanful <ben@benpa.ng>
2025-01-09 18:50:14 +0000

    swayyyy

--- os/loqnux/default.nix
+++ os/loqnux/default.nix
@@ -76,7 +76,7 @@
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd 'sway --unsupported-gpu'";
           user = "greeter";
         };
       };
