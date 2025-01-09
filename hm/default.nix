dbaa37e88565135c40a293f1a8c2fb9e479d32d9
molarmanful <ben@benpa.ng>
2025-01-09 17:32:23 +0000

    home

--- hm/default.nix
+++ hm/default.nix
@@ -8,45 +8,46 @@
     ./nixcats.nix
   ];
 
-  home.username = "ben";
-  home.homeDirectory = "/home/ben";
+  home = {
+    username = "ben";
+    homeDirectory = "/home/ben";
+    stateVersion = "24.11";
 
-  home.stateVersion = "24.11";
-
-  home.packages = with pkgs; [
-    zip
-    xz
-    unzip
-    p7zip
-    bat
-    ripgrep
-    repgrep
-    fzf
-    file
-    fd
-    which
-    tree
-    gnused
-    gnutar
-    gawk
-    jq
-    zstd
-    gnupg
-    nix-output-monitor
-    nix-du
-    btop
-    iotop
-    iftop
-    strace
-    ltrace
-    lsof
-    sysstat
-    lm_sensors
-    ethtool
-    pciutils
-    usbutils
-    nmcli
-  ];
+    packages = with pkgs; [
+      zip
+      xz
+      unzip
+      p7zip
+      bat
+      ripgrep
+      repgrep
+      fzf
+      file
+      fd
+      which
+      tree
+      gnused
+      gnutar
+      gawk
+      jq
+      zstd
+      gnupg
+      nix-output-monitor
+      nix-du
+      btop
+      iotop
+      iftop
+      strace
+      ltrace
+      lsof
+      sysstat
+      lm_sensors
+      ethtool
+      pciutils
+      usbutils
+      nmcli
+    ];
+  };
 
   programs.home-manager.enable = true;
 }
