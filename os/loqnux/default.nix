1970820d21c272c3f521d70da285c3507f6c39ed
Ben Pang <ben@benpa.ng>
2025-01-10 22:11:16 +0000

    screenshare

--- os/loqnux/default.nix
+++ os/loqnux/default.nix
@@ -158,4 +158,13 @@
     enable = true;
     polkitPolicyOwners = [ "ben" ];
   };
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    config.common.default = "*";
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
+  };
 }
