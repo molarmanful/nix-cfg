b852a8df071052ec336705975ebea79324576cce
molarmanful <ben@benpa.ng>
2025-01-12 22:30:36 -0500

    more

--- hm/loqnux/default.nix
+++ hm/loqnux/default.nix
@@ -6,17 +6,32 @@
     ../wezterm
   ];
 
-  home.packages = with pkgs; [
-    floorp
-    chromium
-    libsForQt5.qt5ct
-    discord-canary
-    obsidian
-    zoom-us
-    distrobox
-    azure-cli
-    sops
-  ];
+  home = {
+    packages = with pkgs; [
+      floorp
+      chromium
+      libsForQt5.qt5ct
+      discord-canary
+      obsidian
+      zoom-us
+      distrobox
+      azure-cli
+      sops
+      obs-studio
+      obs-studio-plugins.wlrobs
+    ];
+
+    pointerCursor = {
+      gtk.enable = true;
+      package = pkgs.simp1e-cursors;
+      name = "Simp1e-Dark";
+      size = 24;
+      x11 = {
+        enable = true;
+        defaultCursor = "Simp1e-Dark";
+      };
+    };
+  };
 
   programs.ssh = {
     enable = true;
@@ -25,4 +40,5 @@
           IdentityAgent ~/.1password/agent.sock
     '';
   };
+
 }
