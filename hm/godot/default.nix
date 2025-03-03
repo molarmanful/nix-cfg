5f867640fdf30eef2ddff2dbd300e9a6dbddd3ab
molarmanful <ben@benpa.ng>
2025-03-03 16:54:02 -0500

    godot 4.4

--- hm/godot/default.nix
+++ hm/godot/default.nix
@@ -2,4 +2,6 @@
 {
   xdg.configFile."godot/editor_settings-4.3.tres".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-cfg/hm/godot/editor_settings-4.3.tres";
+  xdg.configFile."godot/editor_settings-4.4.tres".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-cfg/hm/godot/editor_settings-4.4.tres";
 }
