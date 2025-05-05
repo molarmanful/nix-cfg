69d61ab1ead00442e8025bf2b97fa06387afd477
molarmanful <ben@benpa.ng>
2025-05-05 17:31:48 -0400

    fix hunk.nvim deps

--- hm/common/nvim/lua/plugins/init.lua
+++ hm/common/nvim/lua/plugins/init.lua
@@ -199,6 +199,10 @@ return {
   {
     'julienvincent/hunk.nvim',
     cmd = { 'DiffEditor' },
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
     config = function()
       require('hunk').setup()
     end,
