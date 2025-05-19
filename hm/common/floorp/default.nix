9850e416ea06b5c7639583a91b3ac5739403eeec
molarmanful <ben@benpa.ng>
2025-05-19 15:36:06 -0400

    floorp settings

--- hm/common/floorp/default.nix
+++ hm/common/floorp/default.nix
@@ -1,6 +1,105 @@
 {
   programs.floorp = {
     enable = true;
+
+    profiles.default = {
+      id = 0;
+
+      settings = {
+        # arkenfox
+        "browser.aboutConfig.showWarning" = false;
+        "browser.startup.page" = 0;
+        "browser.startup.homepage" = "about:blank";
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+        "browser.newtabpage.activity-stream.default.sites" = "";
+        "browser.newtabpage.enabled" = false;
+        "app.shield.optoutstudies.enabled" = false;
+        "app.normandy.enabled" = false;
+        "app.normandy.api_url" = "";
+        "geo.provider.ms-windows-location" = false;
+        "geo.provider.use_corelocation" = false;
+        "geo.provider.use_geoclue" = false;
+        "extensions.getAddons.showPane" = false;
+        "extensions.htmlaboutaddons.recommendations.enabled" = false;
+        "browser.discovery.enabled" = false;
+        "browser.shopping.experience2023.enabled" = false;
+        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+        "browser.newtabpage.activity-stream.telemetry" = false;
+        "breakpad.reportURL" = "";
+        "browser.tabs.crashReporting.sendReport" = false;
+        "browser.crashReports.unsubmittedCheck.autoSubmit2" = false;
+        "captivedetect.canonicalURL" = "";
+        "network.captive-portal-service.enabled" = false;
+        "network.connectivity-service.enabled" = false;
+        "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
+        "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+        "browser.search.suggest.enabled" = true;
+        "browser.urlbar.suggest.searches" = true;
+        "browser.urlbar.trending.featureGate" = false;
+        "security.ssl.require_safe_negotiation" = true;
+        "security.tls.enable_0rtt_data" = false;
+        "security.OCSP.enabled" = 1;
+        "security.OCSP.require" = true;
+        "security.cert_pinning.enforcement_level" = 2;
+        "security.remote_settings.crlite_filters.enabled" = true;
+        "security.pki.crlite_mode" = 2;
+        "dom.security.https_only_mode" = true;
+        "dom.security.https_only_mode.upgrade_local" = true;
+        "security.ssl.treat_unsafe_negotiation_as_broken" = true;
+        "browser.xul.error_pages.expert_bad_cert" = true;
+        "network.http.referer.XOriginTrimmingPolicy" = 2;
+        "media.peerconnection.ice.proxy_only_if_behind_proxy" = true;
+        "media.peerconnection.ice.default_address_only" = true;
+        "devtools.debugger.remote-enabled" = false;
+        "permissions.manager.defaultsUrl" = "";
+        "network.IDN_show_punycode" = true;
+        "pdfjs.disabled" = false;
+        "pdfjs.enableScripting" = false;
+        "browser.contentanalysis.enabled" = false;
+        "browser.contentanalysis.default_result" = 0;
+        "extensions.postDownloadThirdPartyPrompt" = false;
+        "datareporting.policy.dataSubmissionEnabled" = false;
+        "datareporting.healthreport.uploadEnabled" = false;
+        "toolkit.telemetry.unified" = false;
+        "toolkit.telemetry.enabled" = false;
+        "toolkit.telemetry.server" = "data:,";
+        "toolkit.telemetry.archive.enabled" = false;
+        "toolkit.telemetry.newProfilePing.enabled" = false;
+        "toolkit.telemetry.shutdownPingSender.enabled" = false;
+        "toolkit.telemetry.updatePing.enabled" = false;
+        "toolkit.telemetry.bhrPing.enabled" = false;
+        "toolkit.telemetry.firstShutdownPing.enabled" = false;
+        "toolkit.telemetry.coverage.opt-out" = true;
+        "toolkit.coverage.opt-out" = true;
+        "toolkit.coverage.endpoint.base" = "";
+
+        # personal
+        "browser.tabs.inTitlebar" = 1;
+        "browser.tabs.loadInBackground" = false;
+        "browser.taskbar.previews.enable" = true;
+        "browser.theme.toolbar-theme" = 0;
+        "browser.toolbars.bookmarks.visibility" = "never";
+        "browser.urlbar.shortcuts.bookmarks" = false;
+        "browser.urlbar.showSearchSuggestionsFirst" = false;
+        "floorp.Tree-type.verticaltab.optimization" = true;
+        "floorp.browser.sidebar.enable" = false;
+        "floorp.browser.sidebar.is.displayed" = false;
+        "floorp.browser.sidebar.right" = false;
+        "floorp.browser.tabbar.settings" = 2;
+        "floorp.chrome.theme.mode" = 1;
+        "font.default.x-western" = "sans-serif";
+        "font.name.monospace.x-western" = "Julia Mono";
+        "font.name.sans-serif.x-western" = "SF Pro Display";
+        "font.name.serif.x-western" = "New York Small";
+        "ui.systemUsesDarkTheme" = 1;
+        "userChrome.autohide.back_button" = true;
+        "userChrome.autohide.forward_button" = true;
+        "userChrome.autohide.navbar" = false;
+        "userChrome.autohide.page_action" = true;
+        "userChrome.autohide.sidebar" = false;
+      };
+    };
   };
 
   stylix.targets.floorp.profileNames = [ "default" ];
