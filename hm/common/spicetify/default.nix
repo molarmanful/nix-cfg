8ea8c455052012fdd438aa12d911f033837e566f
molarmanful <ben@benpa.ng>
2025-07-10 13:31:51 -0400

    spicetify sleek

--- hm/common/spicetify/default.nix
+++ hm/common/spicetify/default.nix
@@ -28,24 +28,28 @@
       reddit
     ];
 
-    theme = spicePkgs.themes.dribbblish;
+    theme = spicePkgs.themes.sleek;
     colorScheme = "custom";
     customColorScheme = with scheme; {
       text = base05;
       subtext = base04;
-      sidebar-text = base05;
+      nav-active-text = base0B;
       main = base00;
       sidebar = base00;
       player = base00;
       card = base00;
-      selected-row = base02;
       shadow = base00;
+      main-secondary = base01;
       button = base04;
+      button-secondary = base04;
       button-active = base0B;
       button-disabled = base03;
+      nav-active = base02;
+      play-button = base0B;
       tab-active = base02;
       notification = base02;
       notification-error = base08;
+      playback-bar = base0B;
       misc = base02;
     };
   };
