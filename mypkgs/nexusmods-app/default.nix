d4b5dec15d2ee238ccfe31eff5b937993a17ca4c
molarmanful <ben@benpa.ng>
2025-10-21 09:24:51 -0700

    update nma

--- mypkgs/nexusmods-app/default.nix
+++ mypkgs/nexusmods-app/default.nix
@@ -5,13 +5,13 @@
 }:
 let
   pname = "nexusmods-app";
-  version = "0.18.2";
+  version = "0.19.4";
 in
 appimageTools.wrapType2 {
   inherit pname version;
   src = fetchurl {
     url = "https://github.com/Nexus-Mods/NexusMods.App/releases/download/v${version}/NexusMods.App.x86_64.AppImage";
-    hash = "sha256-bPggrkaLYqUw6scxvV6T/gUIXZkBAxj4iMe47mm3/YU=";
+    hash = "sha256-4f4qEI94uSgMolNyPudCklyZMTe4ZFWpYfITrgTvfic=";
   };
   runtimeInputs = [ _7zz-rar ];
   extraPkgs =
