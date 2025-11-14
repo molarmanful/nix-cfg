b5c731b38a4d131000540c9a25cb2271ea655e3a
molarmanful <ben@benpa.ng>
2025-11-13 23:47:35 -0800

    rename system

--- hm/common/nixcats/default.nix
+++ hm/common/nixcats/default.nix
@@ -3,7 +3,7 @@
   home =
     let
       nvim-base = import ../nvim { inherit inputs; };
-      nvim = nvim-base.packages.${pkgs.system}.default;
+      nvim = nvim-base.packages.${pkgs.stdenv.hostPlatform.system}.default;
     in
     {
       packages = [
