ef71f135c73fca68d36fd4f092d7918bbff6dccd
molarmanful <ben@benpa.ng>
2025-12-29 22:22:46 -0500

    format on save

--- hm/common/nvim/lua/configs/null-ls.lua
+++ hm/common/nvim/lua/configs/null-ls.lua
@@ -3,6 +3,8 @@ local null_ls = require 'null-ls'
 local form = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
 
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+
 null_ls.setup {
   sources = {
     form.stylua,
@@ -32,4 +34,17 @@ null_ls.setup {
     lint.staticcheck,
     lint.credo,
   },
+
+  on_attach = function(client, bufnr)
+    if client.supports_method 'textDocument/formatting' then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { async = false }
+        end,
+      })
+    end
+  end,
 }
