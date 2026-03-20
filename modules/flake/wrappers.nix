ef959f1feb00637aadf078b6a4989a4f8d8109d4
molarmanful <ben@benpa.ng>
2026-03-19 21:03:50 -0700

    refactor: auto-gen wrappers

--- modules/flake/wrappers.nix
+++ modules/flake/wrappers.nix
@@ -1,10 +1,16 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 {
   imports = [
     inputs.wrappers.flakeModules.wrappers
   ];
 
-  flake.wrappers = {
-    neovim = import ../neovim inputs;
-  };
+  flake.wrappers = lib.pipe ../wrappers [
+    builtins.readDir
+    builtins.attrNames
+    (builtins.map (name: {
+      inherit name;
+      value = import ../wrappers/${name} inputs;
+    }))
+    builtins.listToAttrs
+  ];
 }
