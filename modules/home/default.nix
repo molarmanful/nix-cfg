f7a31a62ddf1a7ee1f023d81961554f5da763b19
molarmanful <ben@benpa.ng>
2026-03-22 16:59:35 -0700

    refactor: flatten

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -17,53 +17,51 @@ in
     ./bacon
   ];
 
-  home = {
-    packages = with pkgs; [
-      bashInteractive
-      zip
-      xz
-      unzip
-      p7zip
-      bat
-      ripgrep
-      repgrep
-      fzf
-      file
-      fd
-      which
-      tree
-      gnused
-      gnutar
-      gawk
-      jq
-      zstd
-      gnupg
-      nix-output-monitor
-      nix-du
-      btop
-      iotop
-      iftop
-      strace
-      ltrace
-      lsof
-      sysstat
-      lm_sensors
-      ethtool
-      pciutils
-      usbutils
-      just
-      bottom
-      fastfetch
-      dasel
-      bitwarden-cli
-      bws
-      age
-      sqlite
-      distrobox
-      azure-cli
-      sops
-    ];
-  };
+  home.packages = with pkgs; [
+    bashInteractive
+    zip
+    xz
+    unzip
+    p7zip
+    bat
+    ripgrep
+    repgrep
+    fzf
+    file
+    fd
+    which
+    tree
+    gnused
+    gnutar
+    gawk
+    jq
+    zstd
+    gnupg
+    nix-output-monitor
+    nix-du
+    btop
+    iotop
+    iftop
+    strace
+    ltrace
+    lsof
+    sysstat
+    lm_sensors
+    ethtool
+    pciutils
+    usbutils
+    just
+    bottom
+    fastfetch
+    dasel
+    bitwarden-cli
+    bws
+    age
+    sqlite
+    distrobox
+    azure-cli
+    sops
+  ];
 
   programs = {
     home-manager.enable = true;
